bf6365746faa2fadd98f49d380798d35ba1ffe7c
Alfonso Troya <alfonso.troya@gmail.com>
2018-06-03 23:18:09 +0200

    packages rename

--- imcoders_reader/nodes/imcoders_reader_node.cpp
+++ imcoders_reader/nodes/imcoders_reader_node.cpp
@@ -1,15 +1,15 @@
-#include "imcoder_reader.h"
+#include "imcoders_reader.h"
 #include "ros/ros.h"
 
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "imcoder_reader");
+    ros::init(argc, argv, "imcoders_reader");
 
     ros::NodeHandle nh;
     ros::NodeHandle nh_private("~");
 
-    imcoder_reader::IMCoder imcoderReader(nh, nh_private);
+    imcoders_reader::IMCoder imcoderReader(nh, nh_private);
 
     imcoderReader.spin();
 
