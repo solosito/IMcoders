33e1a153a34eea7bbd986d6a35ccc82236a396de
alfonso.troya <alfonso.troya@gmail.com>
2018-09-19 15:54:44 +0200

    Fix tf exception when no tf in IMU_transformer tool

--- imcoders_diff_odom/scripts/imu_transformer.py
+++ imcoders_diff_odom/scripts/imu_transformer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import rospy
-
 import tf
 import numpy
 from tf.transformations import quaternion_multiply, quaternion_conjugate
@@ -23,20 +22,21 @@ def ImuCallback(data):
     input_q.header.frame_id = origin_frame
     input_q.quaternion = data.orientation
 
-    rotation_q = tf.transformQuaternion(destination_frame, input_q)
+    if(tf.canTransform(destination_frame, origin_frame, rospy.Time(0))):
+        rotation_q = tf.transformQuaternion(destination_frame, input_q)
 
-    rotation_q_array = fromQuaternionStampedToNumpy(rotation_q)
-    input_q_array = fromQuaternionStampedToNumpy(input_q)
+        rotation_q_array = fromQuaternionStampedToNumpy(rotation_q)
+        input_q_array = fromQuaternionStampedToNumpy(input_q)
 
-    rotated_q_array = quaternion_multiply(quaternion_multiply(rotation_q_array, input_q_array),quaternion_conjugate(rotation_q_array))
-    rotated_q = Quaternion(*rotated_q_array)
+        rotated_q_array = quaternion_multiply(quaternion_multiply(rotation_q_array, input_q_array),quaternion_conjugate(rotation_q_array))
+        rotated_q = Quaternion(*rotated_q_array)
 
-    newImu = Imu()
-    newImu.header.frame_id = destination_frame
-    newImu.header.stamp = rospy.Time.now()
-    newImu.orientation = rotated_q
+        newImu = Imu()
+        newImu.header.frame_id = destination_frame
+        newImu.header.stamp = rospy.Time.now()
+        newImu.orientation = rotated_q
 
-    imu_msg_publisher.publish(newImu)
+        imu_msg_publisher.publish(newImu)
 
 if __name__ == '__main__':
     rospy.init_node('imu_msg_transformer', anonymous = True)
@@ -49,7 +49,9 @@ if __name__ == '__main__':
     rospy.Subscriber(i_topic, Imu, ImuCallback)
 
     imu_msg_publisher = rospy.Publisher(o_topic, Imu, queue_size=1)
+
     tf = tf.TransformListener()
+
     rospy.loginfo("Node %s started", rospy.get_caller_id())
     rospy.loginfo("Subscribing to topic: %s", i_topic)
     rospy.loginfo("Publishing into topic: /%s", o_topic)
